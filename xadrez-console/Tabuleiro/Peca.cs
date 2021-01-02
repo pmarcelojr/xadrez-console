6e27693deb0c54def0b7da503e9b8777a231fc07
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-02 12:44:26 -0300

    Created Peca class

---
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+using tabuleiro.Enums;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; protected set; } 
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        {
+            Posicao = posicao;
+            Tabuleiro = tabuleiro;
+            Cor = cor;
+            QtdMovimentos = 0;
+        }
+    }
+}
