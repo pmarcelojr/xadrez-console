1fccad2189a384b0c31c99ce9f67ecce8a1842a9
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-02 12:46:20 -0300

    Start implement to tabuleiro class

--- xadrez-console/Tabuleiro/Peca.cs
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -4,7 +4,7 @@ namespace tabuleiro
 {
     class Peca
     {
-        public Posicao posicao { get; set; }
+        public Posicao Posicao { get; set; }
         public Cor Cor { get; protected set; }
         public int QtdMovimentos { get; protected set; } 
         public Tabuleiro Tabuleiro { get; protected set; }
