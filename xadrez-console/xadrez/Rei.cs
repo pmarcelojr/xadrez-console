5c0cb00baa780c520ab2cddbd32dc27b69eb7515
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-03 13:38:24 -0300

    Created Pecas Rei and Torre

---
+++ xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,17 @@
+using tabuleiro;
+using tabuleiro.Enums;
+
+namespace xadrez 
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
---
+++ xadrez-console/xadrez/Torre.cs
@@ -0,0 +1,17 @@
+using tabuleiro;
+using tabuleiro.Enums;
+
+namespace xadrez
+{
+    class Torre : Peca
+    {
+        public Torre(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
