d56e044bfd80de7993e3ae98f0696a6207bc4214
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-04 15:54:22 -0300

    implementing the Rei possible moves

--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -13,5 +13,59 @@ namespace xadrez
         {
             return "R";
         }
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = Tabuleiro.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+            Posicao pos = new Posicao(0,0);
+
+            // Acima
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            // Nordeste
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            // Direita
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+            
+            // Sudeste
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            // Abaixo
+            pos.DefinirValores(Posicao.Linha + 1, pos.Coluna);
+            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            // Sudoeste
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+            
+            // Esquerda
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
+            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+            
+            // Noroeste
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            return mat;
+        }
     }
 }
