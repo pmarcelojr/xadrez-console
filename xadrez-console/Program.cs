1e9db7ea2c7d783ad652a23d143cc2e0457c8f67
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-04 14:35:16 -0300

    creating looping with user interaction

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -12,8 +12,21 @@ namespace xadrez_console
             try
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
+                
+                while (!partida.Terminada)
+                {
+                    Console.Clear();
+                    Tela.ImprimirTabuleiro(partida.Tab);
 
-                Tela.ImprimirTabuleiro(partida.Tab);
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+
+                    partida.ExecutarMovimento(origem, destino);
+                }
+                
             }
             catch (TabuleiroException e)
             {
