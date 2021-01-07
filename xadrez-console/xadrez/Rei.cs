fb81ddba004214c142f97a71ca27ea58bc7139f3
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-07 18:54:12 -0300

    Implementing special roque grande cast

--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -1,7 +1,7 @@
 using tabuleiro;
 using tabuleiro.Enums;
 
-namespace xadrez 
+namespace xadrez
 {
     class Rei : Peca
     {
@@ -31,63 +31,74 @@ namespace xadrez
         public override bool[,] MovimentosPossiveis()
         {
             bool[,] mat = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
-            Posicao pos = new Posicao(0,0);
+            Posicao pos = new Posicao(0, 0);
 
             // Acima
             pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
                 mat[pos.Linha, pos.Coluna] = true;
 
             // Nordeste
             pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
                 mat[pos.Linha, pos.Coluna] = true;
 
             // Direita
             pos.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
                 mat[pos.Linha, pos.Coluna] = true;
-            
+
             // Sudeste
             pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
                 mat[pos.Linha, pos.Coluna] = true;
 
             // Abaixo
             pos.DefinirValores(Posicao.Linha + 1, pos.Coluna);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
                 mat[pos.Linha, pos.Coluna] = true;
 
             // Sudoeste
             pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
                 mat[pos.Linha, pos.Coluna] = true;
-            
+
             // Esquerda
             pos.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
                 mat[pos.Linha, pos.Coluna] = true;
-            
+
             // Noroeste
             pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
                 mat[pos.Linha, pos.Coluna] = true;
 
             // # JogadaEspecial Roque
-            if(QtdMovimentos == 0 && !Partida.Xeque)
+            if (QtdMovimentos == 0 && !Partida.Xeque)
             {
                 // # JogadaEspecial Roque pequeno
                 Posicao posT1 = new Posicao(Posicao.Linha, Posicao.Coluna + 3);
-                if(TesteTorreParaRoque(posT1))
+                if (TesteTorreParaRoque(posT1))
                 {
                     Posicao p1 = new Posicao(Posicao.Linha, Posicao.Coluna + 1);
                     Posicao p2 = new Posicao(Posicao.Linha, Posicao.Coluna + 2);
-                    if(Tabuleiro.Peca(p1) == null && Tabuleiro.Peca(p2) == null)
+                    if (Tabuleiro.Peca(p1) == null && Tabuleiro.Peca(p2) == null)
                     {
                         mat[Posicao.Linha, Posicao.Coluna + 2] = true;
                     }
                 }
-
+                // # JogadaEspecial Roque Grande
+                Posicao posT2 = new Posicao(Posicao.Linha, Posicao.Coluna - 4);
+                if (TesteTorreParaRoque(posT2))
+                {
+                    Posicao p1 = new Posicao(Posicao.Linha, Posicao.Coluna - 1);
+                    Posicao p2 = new Posicao(Posicao.Linha, Posicao.Coluna - 2);
+                    Posicao p3 = new Posicao(Posicao.Linha, Posicao.Coluna - 3);
+                    if (Tabuleiro.Peca(p1) == null && Tabuleiro.Peca(p2) == null && Tabuleiro.Peca(p3) == null)
+                    {
+                        mat[Posicao.Linha, Posicao.Coluna - 2] = true;
+                    }
+                }
             }
 
             return mat;
