af7925ebfa972765aca67fa44891c81d0ce2dac3
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-07 10:58:50 -0300

    Created a method PodeMoverPara in Peca class

--- xadrez-console/Tabuleiro/Peca.cs
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -38,6 +38,11 @@ namespace tabuleiro
             return false;
         }
 
+        public bool PodeMoverPara(Posicao pos)
+        {
+            return MovimentosPossiveis()[pos.Linha, pos.Coluna];
+        }
+
         public abstract bool[,] MovimentosPossiveis();
     }
 }
